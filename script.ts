cc880b4a159059eb282b7efad699bd853110501f
h45te <104975590+h45te@users.noreply.github.com>
2026-09-19 15:44:01 +0900

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,29 +1,7 @@
 declare var scrapbox: any;
 
-function addCopyButton() {
-	const div = document.createElement('div');
-	const btn = document.createElement('button');
-    const i = document.createElement('i');
-    div.appendChild(btn).appendChild(i);
-
-    btn.classList.add('tool-btn');
-    btn.type = 'button';
-    btn.id = 'copy-as-bracket';
-    btn.ariaLabel = 'Copy as bracket';
-    i.classList.add('kamon', 'kamon-copy');
-
-    btn.addEventListener('click', () => {
-        navigator.clipboard.writeText(`[${new URL(document.URL).pathname}]`);
-    });
-
-    const container = document.getElementsByClassName('page-menu')[0];
-    container.appendChild(div);
-}
-
-function check() {
-    if (scrapbox.Layout === 'page' && document.getElementById('copy-as-bracket') === null) {
-        addCopyButton();
-    }
-}
-scrapbox.on('layout:changed', check);
-check();
+scrapbox.PageMenu.addMenu({
+  title: 'CopyAsBracket',
+  icon: 'kamon kamon-copy',
+  onClick: () => {navigator.clipboard.writeText(`[${decodeURIComponent(new URL(document.URL).pathname)}]`)}
+});
